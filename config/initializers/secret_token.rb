533cb8dd4048bc0e46eb6320e187a097530b60c3
Joe Mellin <mellin.joe@gmail.com>
2013-02-28 21:40:51 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sinterest::Application.config.secret_token = '5375102ee928ac78e2c7301ca9d0cfe24ac881b9a2b25a401851e2e55549ee5006cd3dde6fbb872e3e299739b6c455100c3ffd70829d4774293852b07b8d2092'
