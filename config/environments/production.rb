55f7516b9d1f58aeae22de8fa720ccddb4ee43c2
Joe Mellin <mellin.joe@gmail.com>
2013-03-10 22:16:06 +0800

    aws3 changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,9 +69,9 @@ Sinterest::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => "pinterest-joe-new",
+      :access_key_id => "AKIAIRSFWCTPJJPCEMLA",
+      :secret_access_key => "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"
     }
   }
 
